ee09e4e12bb7ec42fba66ce893a0bb5d954e723a
shams-rasulzade <it.is.shams@gmail.com>
2022-03-10 23:53:45 +0400

    refactor: code for department area

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Dorm_Registration
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Registration());
         }
     }
 }
---
+++ Registration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Dorm_Registration
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-PE9P8LD;Initial Catalog=dorm_registration;Integrated Security=True");
+
+
+
+
+        
+        private void Registration_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select name from department", connection);
+            SqlDataReader read = command.ExecuteReader();
+
+            while(read.Read())
+            {
+                comboBox1.Items.Add(read[0].ToString());
+            }
+            connection.Close();
+
+        }
+    }
+}
